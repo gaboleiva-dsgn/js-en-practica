e10d0da46c708fa30b518a94f3a09b53a97b5775
Gabriel Leiva <g.leivach@gmail.com>
2024-02-18 04:36:34 -0300

    Finalización del ejercicio 2 =(

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,7 +1,46 @@
-let btnUno = document.getElementById("btn-1");
+// se declaran las variables para su manipulación
+const box = document.getElementById('caja');
+const btnUno = document.getElementById('btn-1');
+const btnDos = document.getElementById('btn-2');
+const btnTres = document.getElementById('btn-3');
+const btnCuatro = document.getElementById('btn-4');
+const btnCinco = document.getElementById('btn-5');
+const btnSeis = document.getElementById('btn-6');
 
+// con el evento addEventListener (click) escuchamos al evento click para llamar a la función
+btnUno.addEventListener("click", function () {
+    // Le cambiamos valor backgroun-color a la caja
+    // !!Profesor quisiera que me explicara por qué no funciona este metodo, el que sigue a continuación y está comentado.
+    //document.getElementById('box').setAttribute('style', 'background-color: #e53e3e');
+    box.style.backgroundColor = "#e53e3e";
+});
 
-btnUno.addEventListener( "click", function()
-{
-    document.getElementById("caja").setAttribute('style', 'background-color: #e53e3e');
+// con el evento addEventListener (click) escuchamos al evento click para llamar a la función
+btnDos.addEventListener("click", function () {
+    // Le cambiamos valor backgroun-color a la caja
+    box.style.backgroundColor = "#dd6b20";
+});
+
+// con el evento addEventListener (click) escuchamos al evento click para llamar a la función
+btnTres.addEventListener("click", function () {
+    // Le cambiamos valor backgroun-color a la caja
+    box.style.backgroundColor = "#faf089";
+});
+
+// con el evento addEventListener (click) escuchamos al evento click para llamar a la función
+btnCuatro.addEventListener("click", function () {
+    // Le cambiamos valor backgroun-color a la caja
+    box.style.backgroundColor = "#48bb78";
+});
+
+// con el evento addEventListener (click) escuchamos al evento click para llamar a la función
+btnCinco.addEventListener("click", function () {
+    // Le cambiamos valor backgroun-color a la caja
+    box.style.backgroundColor = "#81e6d9";
+});
+
+// con el evento addEventListener (click) escuchamos al evento click para llamar a la función
+btnSeis.addEventListener("click", function () {
+    // Le cambiamos valor backgroun-color a la caja
+    box.style.backgroundColor = "#d53f8c";
 });
