ce002730f5d496323ac190309440614f75483b7b
Gabriel Leiva <g.leivach@gmail.com>
2024-02-18 04:07:06 -0300

    Carga parcial del ejercicio 2

---
+++ assets/js/script2.js
@@ -0,0 +1,7 @@
+let btnUno = document.getElementById("btn-1");
+
+
+btnUno.addEventListener( "click", function()
+{
+    document.getElementById("caja").setAttribute('style', 'background-color: #e53e3e');
+});
